f4cde5a1b63022a9bfe394400013ce66fc57c7c1
Sch39 <sukronraezpactoer@gmail.com>
2020-11-28 11:07:44 +0700

    no 1 selesai

--- 1.js
+++ 1.js
@@ -1,1 +1,24 @@
-console.log("hello");
+let detailKaloriAndi = (jumlah) => {
+  let jenisOlahraga;
+  var waktuOlahraga = Math.round((jumlah / 20) * 2);
+  if (jumlah > 750) {
+    jenisOlahraga = "Lari";
+  }
+  else if (jumlah > 500) {
+    jenisOlahraga = "Badminton";
+  }
+  else if (jumlah <= 500) {
+    jenisOlahraga = "Renang";
+  }
+  else {
+    jenisOlahraga = "Tidak Diketahui";
+  }
+  return `Jenis Olahraga : ${jenisOlahraga}
+  Jumlah Kalori : ${jumlah} Kalori
+  Waktu Olahraga : ${waktuOlahraga} Menit`;
+}
+
+// contoh penampilan hasil function
+console.log(detailKaloriAndi(751));
+console.log(detailKaloriAndi(750));
+console.log(detailKaloriAndi(200));
